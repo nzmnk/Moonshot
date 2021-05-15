eb5304e4797424f784712f650442c2fa440c15cc
Nikita Zimenko <nic.zimen@gmail.com>
2021-05-15 11:41:31 +0300

    initial commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Никита Зименко on 15.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
