f94c3dd5616002f35045d42d3f2a9603dac34441
Nikita Zimenko <nic.zimen@gmail.com>
2021-05-15 23:20:50 +0300

    added image assets and json + dependencies

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  
+//
+//  Created by Никита Зименко on 15.05.2021.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
